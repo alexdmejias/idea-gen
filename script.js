15185ca0bf63f4587b5668d752541742777c0ab6
Alex Mejias <alme1304@gmail.com>
2012-05-30 13:41:51 -0400

    responsive :tablets

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
 		});
 
 		$(window).on('load',function(){
-			var butt_text = ["Change the World",'Make a million','Be famous'];
+			var butt_text = ["Change the World",'Make a million','Be famous','Start a Business','Make a startup','Do something','New weekend project'];
 			$('#button').attr('value',butt_text[Math.floor(Math.random()*butt_text.length)]);
 		});
 
